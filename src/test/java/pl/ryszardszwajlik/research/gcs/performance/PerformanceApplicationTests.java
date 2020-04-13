fe7a39cae8245da45f6090cad66c8b189dfc0dd0
Ryszard Szwajlik <rszwajlik@rocketlawyer.co.uk>
2020-04-13 13:52:04 +0200

    Initial commit, src generated with Spring Initializr

---
+++ src/test/java/pl/ryszardszwajlik/research/gcs/performance/PerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ryszardszwajlik.research.gcs.performance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
